96815ba4a8664b4d9e3cdab966b4c887e2e093a2
Ilan Rojas <bobatthevelvetroom@gmail.com>
2023-03-31 13:32:00 -0400

    test 2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let initpage = document.querySelector("#initial-page")
 let questionContainer = document.querySelector("#question-container")
+let answerDiv = document.querySelector("#answers")
+let answerbuttons = document.querySelector("#answerbuttons")
 
 fetch('http://localhost:3000/trivia')
     .then((res) => res.json())
     .then((data) => console.log(data))
 
-stateOne()
+
 function stateOne(){
     initpage.style.display = 'none'
 }

01196c36b2c0f037cd2257c0389193590b9a5aa0
Ilan Rojas <bobatthevelvetroom@gmail.com>
2023-04-01 23:43:50 -0400

    Should be fully randomized array with no dupes, hallelujah

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ let button = document.querySelector("#start")
 
 const apiUrl = 'http://localhost:3000/trivia'
 let questionsA = []
+let questionsB = []
+
+let questionAt = 0;
 
 fetch(apiUrl)
     .then((res) => res.json())
@@ -44,7 +47,8 @@ function startTriviaState(){
     questionContainer.style.display = 'block'
     answerDiv.style.display = 'block'
     answerbuttons.style.display = 'block'
-    setTheQuestion()
+    randomizeArray()
+   // setTheQuestion()
 }
 
 
@@ -56,15 +60,48 @@ function resultsState(){
     resultsPage.style.display = 'block'
 
     resultsPage.addEventListener('click', () =>{
-        startTriviaState()                  //CLICK THE FAIL WHEN YOU SEE ME TEXT
+        startTriviaState()
+                       //CLICK THE FAIL WHEN YOU SEE ME TEXT
     })
+}
+let randomNumber = () => {
+    min = 0
+    max = questionsA.length - 1
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function randomizeArray(){
+    questionsB = [];
+    console.log(randomNumber())
+    let numberArray = [];
+    for (let index = 0; index < 10; index++) {    /// Sets up an array of random numbers
+        numberArray.push(randomNumber())            /// gives numberArray a new number each iteration
+    }
+    console.log(`FIRST ARRAY: ${numberArray}`)            //proof that it works
+    for (let index = 0; index <= numberArray.length; index++) {
+        for (let DupeCheck = 0; DupeCheck <= numberArray.length ;DupeCheck++) {   ///looping through the array for any dupes
+            while(numberArray[index] == numberArray[DupeCheck] && index != DupeCheck){ //if 2 numbers are the same..
+                numberArray[index] = randomNumber()       // give the latest number a new number
+                console.log('Had to change a number')           //for sanity purposes
+            }
+        }
+
+    }
+    console.log(`SECOND ARRAY: ${numberArray}`)
+
+    for (let index = 0; index < numberArray.length ; index++) {
+        questionsB.push(questionsA[numberArray[index]])
+
+    }
+    console.log(questionsB)
+
 }
 
 function setTheQuestion(){ //This is a lot to digest so take it slow.
-    questionContainer.textContent = questionsA[0].question //Currently sets the first question from the array, will change later
+
+    questionContainer.textContent = questionsB[questionAt].question //Currently sets the first question from the array, will change later
 
     let buttonArray = Array.from(answerbuttons.children) //make the children of the answerbuttons into an array
-    let answers = questionsA[0].answers //make the answers from the question into an object
+    let answers = questionsB[questionAt].answers //make the answers from the question into an object
 
     Object.keys(answers).forEach((key, aIndex) => { //get the answer object and loop through it and give it the key and index as the parameter
 
@@ -83,6 +120,4 @@ function setTheQuestion(){ //This is a lot to digest so take it slow.
            console.log(button.textContent)  //for sanity purposes
         })
     })
-
-
 }
